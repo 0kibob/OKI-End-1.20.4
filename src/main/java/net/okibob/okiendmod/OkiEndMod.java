442682dc325a61009575a4712664c0614964d87b
0kibob <Dragonsonix2@gmail.com>
2024-04-30 22:04:47 +0200

    Important Changes Made!

--- src/main/java/net/okibob/okiendmod/OkiEndMod.java
+++ src/main/java/net/okibob/okiendmod/OkiEndMod.java
@@ -18,9 +18,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(OkiEndMod.MOD_ID)
 public class OkiEndMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "okiendmod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
 
